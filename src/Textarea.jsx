81231f77662cfd70b3d2381503f0ded3c5e230bd
Idan Bahar <bahar.idan91@gmail.com>
2025-06-07 10:42:34 +0300

    controlled textarea and some validaiton

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 
 export default function Textarea() {
   const [text, setText] = useState('')
+
+  function handleChange(e) {
+    let newText = e.target.value
+
+    if (newText.includes('<script>')) {
+      alert('No Script tag allowed!')
+      newText = newText.replace('<script>', '')
+    }
+    setText(newText)
+  }
+
   return (
     <textarea
       value={text}
-      onChange={(e) => {
-        const newText = e.target.value
-        setText(newText)
-      }}
+      onChange={handleChange}
       className='textarea'
       placeholder='Enter your text'
       spellCheck='false'
